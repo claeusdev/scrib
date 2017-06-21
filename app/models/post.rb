1ea217c15c2ba023397fd635a7060f86ffc5df2d
Nana Adjei Manu <claeusdev@gmail.com>
2017-06-21 02:15:59 +0000

    Add tags to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,16 +5,27 @@ class Post < ApplicationRecord
 	has_attached_file :photo, styles: { large: "600x400>",medium: "300x300>", thumb: "80x80>" }, default_url: "/images/:style/missing.svg"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
   
-
-  belongs_to :user
-
   default_scope { order("created_at DESC") }
-
+  belongs_to :user
   has_many :comments
-  accepts_nested_attributes_for :comments
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
 
   validates :title, :content, presence: true
   validates :slug, uniqueness: true, presence: true
 
+  def self.tagged_with(name)
+    Tag.find_by(name: name).posts  
+  end
+
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name).first_or_create!
+    end
+  end
+
+  def all_tags
+    tags.map(&:name).join(',')
+  end
   
 end
